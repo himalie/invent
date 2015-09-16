16355a320e09b136eb3ae57e92e3cf08a3af5af1
himalie <himalie1@gmail.com>
2015-09-16 23:02:27 +0530

    init commit

---
+++ app/js/Controller.js
@@ -0,0 +1,7 @@
+angular.module('myApp', [])
+
+.controller('MyController', function($scope) {
+  $scope.person = {
+    name: "Ari Lernerrrr"
+  };
+});
---
+++ app/js/app.js
@@ -0,0 +1,34 @@
+
+angular.module('myApp', ['ngRoute'])
+
+.run(function($rootScope) {
+  $rootScope.name = "Ari Lerner";
+})
+
+.config(function($stateProvider, $RouterProvider, $urlRouterProvider) {
+ 
+  //
+  // Now set up the states
+  /*$stateProvider
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "index.html",
+      controller: 'MyController'
+    })
+    .state('app/home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html",
+          controller: 'HomeController'
+        }
+      }
+    });*/
+
+    /*$routeProvider.when("/home", {template: "templates/home.html" })
+    .otherwise({redirectTo: "/index", template : "index.html"});*/
+     //
+  // For any unmatched url, redirect to /state1
+  //$urlRouterProvider.otherwise("app/index");
+});
